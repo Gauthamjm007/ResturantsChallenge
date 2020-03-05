3108c24a16412f2579b33a2c9a5416b6674fa717
Gauthamjm007 <j.mgautham15@gmail.com>
2020-03-05 21:08:15 +0530

    Updates

---
+++ precache-manifest.431ca317158377bc0eb7c67bfadf2901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c475c1abda8a8554d4c6c3116219b3e",
+    "url": "/ResturantsChallenge/index.html"
+  },
+  {
+    "revision": "23880a8c68231ac2fbef",
+    "url": "/ResturantsChallenge/static/css/main.defb7a05.chunk.css"
+  },
+  {
+    "revision": "9ade729eccf6a9db266d",
+    "url": "/ResturantsChallenge/static/js/2.a5ce5530.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/ResturantsChallenge/static/js/2.a5ce5530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23880a8c68231ac2fbef",
+    "url": "/ResturantsChallenge/static/js/main.ec28a5a9.chunk.js"
+  },
+  {
+    "revision": "c099a3b84168c9493741",
+    "url": "/ResturantsChallenge/static/js/runtime-main.6a7c3d51.js"
+  }
+]);
